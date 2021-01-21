50887338263b2c76bfc89f460e9f18e0c59813e3
Julien <julien.bellanger@cornershopapp.com>
2021-01-21 16:06:28 -0300

    feature (shopper-invoicing-integration): corrections and improvment
     - improve serialization endpoints
     - improve serialization emit notification
     - include template invoice.html

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_invoicing_integration",
-    version="0.dev3",
+    version="0.dev3.1",
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data = {

9de631d216e12ca8419530aa52efcb77cdf4371c
Julien <julien.bellanger@cornershopapp.com>
2021-01-24 15:39:09 -0300

    test email

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_invoicing_integration",
-    version="0.dev4.7.2",
+    version="0.dev4.7.3",
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data = {

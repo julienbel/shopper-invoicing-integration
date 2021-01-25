4c191e268785fd460397a44469aa25c46bc533d0
Julien <julien.bellanger@cornershopapp.com>
2021-01-25 14:16:17 -0300

    corrections

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="shopper_invoicing_integration",
-    version="0.dev6.2",
+    version="0.dev6.3",
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data = {
